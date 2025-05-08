264d095fde3d3ea1f3a7da79bd70d115fef3badd
Kongothebongo <Kongothebongo@users.noreply.github.com>
2025-05-08 11:38:48 +0000

    deploy: dc5a71dfc882621e2ddc1a108cdf56c59eb9804b

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,96 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
-   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
-  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
- / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
-/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
+<div class="custom-text-header"> <pre>                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                         .::.                                                               
+                                                       :$X::$X                                                              
+                                                     .:X::x+X:                                                              
+                                                  .:X+:;;+++                                                                
+                                              ..   +;;+;Xx; .....:::::::.                                                   
+                                           .:+$+$;.X;+;;X&&&&$$$XXXXXXXX$&&X+:.. .                                          
+                                         :XX+X;++X&$x$XXX$$XXXXX$$&&$XXXXXXXX$$$$X:                                         
+                                      .+XXXX+; ::;++XXXXXX$XXXXXXXXXXXXXXXXXXXXX$XX$X;.                                     
+                                     ;$x$X;::. ;;+x$XXX$$XXXXXXXXXXXXXXXXXXXXXXXXX$XXX$x.                                   
+                         .  ..     .;$XX$$x;::::;+XXXX$XxXXXXXXxXXXXXXxXX$XXXXXXXXXX$XXx$;.                                 
+                       . .::::.    :$x$$X$++++$$xXxXX$Xx+xXXXXXxX$XXXXxxxX$XXXXXXXXXX&XXXXx                                 
+                     ..::.....::.  +X$X;+$$x+XXXxXX$&Xxxxxxxxxxxx$XxxxxxxxX$$xXXXXXXXX$XXXX$:                               
+                     .:..:::::.::  +X+;.:+$X$$$XXX$XXXxxXXXXXxXxxX$XxXXXXxxX$XXXXXXXXX$&XXXX$.                              
+                     .:..:.::..::  xX$++;XXX$$XX$; x$XxXXXXXXXXXxX$$XXXXXXXxX&XXXXXXXXX$$XXXXX.                             
+                     .:..;::..::   +$X&:XXX$XX$$ ..$$XX$X$$$$$$XXX$&$X$XXXXXX$$XXXXXXXXX$XXXXX+                             
+                      ....:..:.     .X:XXX$$$$$:. :$$XX$X$$$$$$$$$$$$$$XXX$$XX$XXXXXXXXX$$XXXX$.                            
+                         .:..:.    :$:xXX$$$$$..  :$$$$$XX$$$$$$X$$&$$$$$XX$X$$$XXXXXXXXX$XXXX$x                            
+                          .::::   :X::XXX$$X$+.  ..$$$$$XX$$$$$$X$$&&$$$XXX$$$$&XXXXXXXXX$XXXXX$.                           
+                          .::::  .X..+XX$$$$$ ..  .x$$$$XX$X$$$$XX$$X$$$$XXX$$$&$XXXXXXXX$$XXXX$:                           
+                               . x::.XXX$$$$x.. .;x$$$$$XXXXX$&$$XX$$$$$$XXX$$$&$XXXXXXXXX$XXXX$;                           
+                               .+;:..$X$$$&$;xX;::..x$$$$XXXXX$$$XX$:$$$XXXX$$$&$$X$XXXXXX$XXXX$;                           
+                               :+::.:$X$$$$$+..  .  .X$$$XXXXXX$$XXX:.$$XXXXX$X&X$X$XXXXXX$$XXX$;                           
+                               :X+:.:$$$$$$ :  . ..   X$$+XX$$$$$XXX: :+xXXXX$$Xx$X$$XXXXXX$$XX$;                           
+                                 .;$&$$$X$x.  ...:+;.   ..  .     ..:;: .       +XX$$XXXXXX$$XX$;                           
+                                    $$$$X$: ...+$$$X::.. . .  ......+$$XX$X+:.  +$X$$XXXXXXX$$$&;                           
+                                   .$$$$$$::X$XXxx$$$$.    .  .  .XXXX$&$+X$XXX++XXX$XXXXXXX$&$&;                           
+                                  .;$$$$$$;XXX::$+:;+$ ..  ..  ..:.X+;+++$.::+XX$$XX$$XXXXXXX$$&;                           
+                                  .$$$$$$$X +; ++++;xX .  .  . .  ;++++++x: .:.$XXXX$$XXXXXXXX$&;                           
+                                 .$$$$&$$X$    :++;X;;  .  . .  . :++++++X    . +XXXX&XXXXXXXXX$$.                          
+                                .X$$$&&&$$$+..  ;XX+:....  .  .... :+XxX;    ...+XXXX$$XXXXXXXXX$x.                         
+                              .+$$$&&&$&X$$$;:............  .  .................+XXXXxxXXXxXXX$XX$$;                        
+                            .;$$X$&$$&$$$;X;++............  ....................$XXX$::x$XXxxXX$$XX$+.                      
+                          .:$$XX$&+:$$$$$$;.............. .. ..................$XXX$X:::X$XXx+xXX$$XX$:                     
+                          :$XXX$&+ .$$$$$$&;.............. .  . ..............+XXX$&;:;$$&&XXX++xX$$$$.                     
+                         .XxXXX$x  .+$$$$$$&x. . . . .  ..:;;   . ...........xX$$$&$&$$$$$Xx&$XX+xX$&X.                     
+                        .+XXXXX$:    X$$$$$$$&$;. .  . .  . ..    .  ....::;XX$$&$$$$$$$$$+$$$&$X+x$+.                      
+                         XXXXXX$;     ;$$$$$$&&&&&&X;:.. ..  .......:;;x$&$XXX&$$$$$$$$&&&$$$$$$$&x.                        
+                         :$$XXX$$.     ..+&&$$&X+++;;:+&&&&&&+::+$&&&$&$$$XXX$$$$$$$$$$$$$$$$&&X:                           
+                         .;$$$XXX$$$$.    .+:......:::.;$&&&&&&&&$$$$&&&$X$XX$$$$$$$$$$&&&X+:.                              
+                           .+$&$$$$+.     .xX++Xxx+x++$X$&&&&&&&&$$X+;+&XX$XXX&$$$$XXX$X.                                   
+                                          .$XX:XX++x+XXX$$$&$$$+::;;x:$&$$$$XX$&$$$$XXX$.                                   
+                                         ;x.:XXxX+.+xX$$;;Xx+ :;:.:;+$&&&$$&&$X$$$$$X$X.                                    
+                                         +;;;X+;x;X:+x;;;+xX$$$$$X&&+;:;&$&&&$&$$$$$$&+Xx+X:Xx;+.                           
+                                         ;+++X.x+X+X.X++&&&$$$XXXxxxxX+ ;$&&&$$&$$$$$XXX:X+X:+$x;                           
+                                         +:::$+++++;.xx;.+&$$$$XXxxxxxxx  +:$$$$$&&$X$X&x+xXXx+$:                           
+                                         +;::+++;:;x+.X$$$$$&$$$XXXxxxx$+.+;x$&$$$$$XX$$$Xx$XX$x.                           
+                                         .+++x$$&&&$$$&$:     ;X&$XXX$&X++;+X+     .;:                                      
+                                                                  .::.   ..                                                 
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
+                                                                                                                            
                                                                  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
